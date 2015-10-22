1f230b67d3655adc210d69a9289be7028e502af7
Jérémie Soria <Jeremie.Soria@geemedia.com>
2015-10-22 19:13:44 -0400

    Added a mechanism of delayed garbage collection in the threads of the async task pool. This way we can create autorelease objects from a background thread.

--- cocos/base/CCDirector.cpp
+++ cocos/base/CCDirector.cpp
@@ -114,6 +114,8 @@ bool Director::init(void)
 {
     setDefaultValues();
 
+    _cocos2d_thread_id = std::this_thread::get_id();
+
     // scenes
     _runningScene = nullptr;
     _nextScene = nullptr;
@@ -1380,8 +1382,6 @@ void DisplayLinkDirector::startAnimation()
     }
 
     _invalid = false;
-
-    _cocos2d_thread_id = std::this_thread::get_id();
 
     Application::getInstance()->setAnimationInterval(_animationInterval);
 
