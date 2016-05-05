c25ec19997ab63c59572730f639d2d8c6222e235
Miguel Aguilera <miguel.aguilera@geemedia.com>
2016-05-05 11:27:08 -0400

    Remove useless parenthesis

--- download-deps.py
+++ download-deps.py
@@ -45,7 +45,7 @@ import distutils
 import fileinput
 import json
 import platform
-from helper_download_fmod import (mount_orion_linux, unmount_orion_linux)
+from helper_download_fmod import mount_orion_linux, unmount_orion_linux
 
 from optparse import OptionParser
 from time import time
