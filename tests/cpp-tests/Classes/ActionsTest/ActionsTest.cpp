5ec2059c24661c560c343aefd6a7ec5a1ecafb0f
Charles_St_Laurent_GEE <Charles.St-Laurent@geemedia.com>
2016-08-03 13:35:42 -0400

    FDP-5580 ResizeBy and ResizeTo fix and Test cpp

--- tests/cpp-tests/Classes/ActionsTest/ActionsTest.cpp
+++ tests/cpp-tests/Classes/ActionsTest/ActionsTest.cpp
@@ -26,12 +26,14 @@
 #include "ActionsTest.h"
 #include "../testResource.h"
 #include "cocos2d.h"
+#include "ui/CocosGUI.h"
 
 #include "renderer/CCRenderer.h"
 #include "renderer/CCCustomCommand.h"
 #include "renderer/CCGroupCommand.h"
 
 USING_NS_CC;
+using namespace cocos2d::ui;
 
 ActionsTests::ActionsTests()
 {
@@ -78,6 +80,7 @@ ActionsTests::ActionsTests()
     ADD_TEST_CASE(ActionCardinalSplineStacked);
     ADD_TEST_CASE(ActionCatmullRomStacked);
     ADD_TEST_CASE(PauseResumeActions);
+    ADD_TEST_CASE(ActionResize);
     ADD_TEST_CASE(Issue1305);
     ADD_TEST_CASE(Issue1305_2);
     ADD_TEST_CASE(Issue1288);
@@ -2157,6 +2160,52 @@ std::string PauseResumeActions::title() const
 std::string PauseResumeActions::subtitle() const
 {
     return "All actions pause at 3s and resume at 5s";
+}
+
+//------------------------------------------------------------------
+//
+//    ActionResize
+//    Works on all nodes where setContentSize is effective. 
+//    But it's mostly useful for nodes where 9-slice is enabled
+//
+//------------------------------------------------------------------
+void ActionResize::onEnter() 
+{
+    ActionsDemo::onEnter();
+
+    _grossini->setVisible(false);
+    _tamara->setVisible(false);
+    _kathia->setVisible(false);
+
+    Size widgetSize = getContentSize();
+
+    Text* alert = Text::create("ImageView Content ResizeTo ResizeBy action. \nCan be use on any object affected by the content size.", "fonts/Marker Felt.ttf", 16);
+    alert->setColor(Color3B(159, 168, 176));
+    alert->setPosition(Vec2(widgetSize.width / 2.0f,
+                            widgetSize.height / 2.0f - alert->getContentSize().height * 2.125f));
+
+    addChild(alert);
+
+    // Create the imageview
+    ImageView* imageView = ImageView::create("cocosui/buttonHighlighted.png");
+    imageView->setScale9Enabled(true);
+    imageView->setContentSize(Size(50, 80));
+    imageView->setPosition(Vec2(widgetSize.width / 2.0f,
+                                widgetSize.height / 2.0f));
+
+    auto resizeDown = cocos2d::ResizeTo::create(2.8f, Size(50, 80));
+    auto resizeUp = cocos2d::ResizeTo::create(2.8f, Size(300, 80));
+
+    auto resizeByDown = cocos2d::ResizeBy::create(1.8f, Size(0, -30));
+    auto resizeByUp = cocos2d::ResizeBy::create(1.8f, Size(0, 30));
+    addChild(imageView);
+    auto rep = RepeatForever::create(Sequence::create(resizeUp, resizeDown, resizeByDown, resizeByUp, nullptr));
+    imageView->runAction(rep);
+}
+
+std::string ActionResize::subtitle() const 
+{
+    return "ResizeTo / ResizeBy";
 }
 
 //------------------------------------------------------------------
